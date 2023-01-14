0400fcf0bb24b170aa1bd3b9e2f52e5c92a97ed2
nicompiledev <nsrivera80@misena.edu.co>
2023-01-14 16:05:50 -0500

    Added Contador component with logic to count clicks and reset the counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,31 @@
-import './App.css';
-import Boton from './components/Boton';
-import freeCodeCampLogo from './img/freecodecamp-logo.png';
+import './App.css'; 
+// Se importa el archivo de estilos App.css
+import Boton from './components/Boton'; 
+// Se importa el componente personalizado Boton
+import Contador from './components/Contador'; 
+// Se importa el componente personalizado Contador
+import freeCodeCampLogo from './img/freecodecamp-logo.png'; 
+// Se importa la imagen freeCodeCampLogo
+import { useState } from 'react'; 
+// Se importa el hook useState de la librería React
 
+// Se define una función para el componente App
 function App() {
 
+  // Se utiliza el hook useState para crear una variable de estado llamada numClics, inicialmente en 0
+  const [numClics, setNumClics ] = useState(0);
+
+  // Se define una función manejarClic que aumenta el valor de numClics en 1
   const manejarClic = () => {
-    console.log('Clic');
+    setNumClics(numClics+1);
   };
 
+  // Se define una función reiniciarContador que establece el valor de numClics en 0
   const reiniciarContador = () => {
-    console.log('Reiniciar');
-  }
+    setNumClics(0);
+  };
 
+  // Se retorna un JSX que renderiza una vista para el componente
   return (
     <div className='App'>
       <div className='freecodecamp-logo-contenedor'>
@@ -22,11 +36,15 @@ function App() {
         />
       </div>
       <div className='contenedor-principal'>
+        {/* Se renderiza el componente Contador, pasando como prop el valor de numClics */}
+        <Contador numClics={numClics} />
+        {/* Se renderiza una instancia del componente Boton con el texto 'Clic', y la propiedad esBotonDeClic en true, y el evento onClick atado a la función manejarClic */}
         <Boton
           texto='Clic'
           esBotonDeClic={true}
-          manejarClic={manejarClic}/>
+          manejarClic={manejarClic} />
           
+        {/* Se renderiza una instancia del componente Boton con el texto 'Reinicar' y el evento onClick atado a la función reiniciarContador */}
         <Boton
           texto='Reinicar'
           esBotonDeClic={false}
@@ -36,4 +54,5 @@ function App() {
   );
 }
 
+// Se exporta el componente App para ser utilizado en otras partes de la aplicación
 export default App;
--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+// Se importa React para poder utilizar JSX
 import '../stylesheets/Boton.css';
+// Se importa el archivo de estilos para el componente Boton
 
+// Se define una función para el componente Boton que recibe como parámetros: texto, esBotonDeClic y manejarClic
 function Boton({ texto, esBotonDeClic, manejarClic }) {
+  // Se retorna JSX que representa un botón HTML con una clase dinámica basada en el valor de esBotonDeClic, y un evento onClick atado a la función manejarClic
   return (
     <button 
       className={esBotonDeClic ? "boton-clic" : "boton-reiniciar"}
@@ -11,4 +15,5 @@ function Boton({ texto, esBotonDeClic, manejarClic }) {
   );
 }
 
+// Se exporta el componente Boton para ser utilizado en otras partes de la aplicación
 export default Boton;
